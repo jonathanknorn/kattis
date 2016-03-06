5c42342980605e39b8ec58725d618f63373fbc37
jonathanknorn <j.knorn@gmail.com>
2016-03-06 01:43:59 +0100

    averageeasy refactored

--- averageseasy.cpp
+++ averageseasy.cpp
@@ -10,31 +10,40 @@ using namespace std;
 int main(){
     int t;
     cin >> t;
-    for(int i = 0; i < t; ++i){
-        int n, m, n_sum = 0, m_sum = 0;
+    while (t--) {
+        double n, m, cs_sum = 0, e_sum = 0;
         vector<int> v;
         cin >> n >> m;
-        for(int j = 0; j < n; ++j){
-            int k;
-            cin >> k;
-            v.push_back(k);
-            n_sum += k;
+        
+        //Read the IQ of all computer science and economics students
+        for(int i = 0; i < n; ++i){
+            double iq;
+            cin >> iq;
+            //store CS IQ in vector v
+            v.push_back(iq);
+            //add to sum of CS IQ
+            cs_sum += iq;
         }
-        for(int j = 0; j < m; ++j){
-            int k;
-            cin >> k;
-            m_sum += k;
+        
+        for(int i = 0; i < m; ++i){
+            double iq;
+            cin >> iq;
+            //add to sum of economics IQ
+            e_sum += iq;
         }
-        double n_avg = static_cast<double>(n_sum)/n;
-        double m_avg = static_cast<double>(m_sum)/m;
+        
+        //Calculate IQ averages
+        double cs_avg = cs_sum/n;
+        double e_avg = e_sum/m;
 
-        int sum = 0;
+        int ans = 0;
         for(auto it = v.begin(); it != v.end(); ++it){
-            if(*it < n_avg && *it > m_avg){
-                ++sum;
+            //for each cs student with an iq lower than cs-average and higher than e-average, increment answer
+            if(*it < cs_avg && *it > e_avg){
+                ++ans;
             }
         }
-        cout << sum << endl;
+        cout << ans << endl;
     }
     return 0;
 }
